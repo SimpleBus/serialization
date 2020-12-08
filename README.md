2d551708ceefa0e6399308532a6bc205db423100
Cliff Odijk <cliff@jcid.nl>
2020-12-08 21:12:12 +0100

    Move to `packages/` dir

---
+++ README.md
@@ -0,0 +1,16 @@
+# SimpleBus/Serialization
+
+[![Build Status](https://travis-ci.org/SimpleBus/Serialization.svg?branch=master)](https://travis-ci.org/SimpleBus/Serialization)
+
+By [Matthias Noback](http://php-and-symfony.matthiasnoback.nl/), Cliff Odijk, Ruud Kamphuis
+
+This package contains generic classes and interfaces which can be used to serialize [SimpleBus](https://github.com/SimpleBus/MessageBus) messages.
+
+[Read the full documentation here](http://simplebus.github.io/Serialization)
+
+Resources
+---------
+
+  * [Report issues](https://github.com/SimpleBus/SimpleBus/issues) and
+    [send Pull Requests](https://github.com/SimpleBus/SimpleBus/pulls)
+    in the [main SimpleBus repository](https://github.com/SimpleBus/SimpleBus)
---
+++ tests/NativeObjectSerializerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SimpleBus\Serialization\Tests;
+
+use PHPUnit\Framework\TestCase;
+use SimpleBus\Serialization\Envelope\DefaultEnvelope;
+use SimpleBus\Serialization\NativeObjectSerializer;
+use SimpleBus\Serialization\Tests\Fixtures\AnotherDummyMessage;
+
+class NativeObjectSerializerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_serialize_a_default_message_envelope_with_a_serialized_message()
+    {
+        $envelope = DefaultEnvelope::forSerializedMessage(
+            'SimpleBus\Serialization\Tests\Fixtures\DummyMessage',
+            'serialized message'
+        );
+        $serializer = new NativeObjectSerializer();
+
+        $serializedEnvelope = $serializedEnvelope = $serializer->serialize($envelope);
+        $this->assertIsString($serializedEnvelope);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_serialize_and_deserialize_a_default_message_envelope_with_a_serialized_message()
+    {
+        $originalEnvelope = DefaultEnvelope::forSerializedMessage(
+            'SimpleBus\Serialization\Tests\Fixtures\DummyMessage',
+            'serialized message'
+        );
+        $serializer = new NativeObjectSerializer();
+
+        $serializedEnvelope = $serializedEnvelope = $serializer->serialize($originalEnvelope);
+        $deserializedEnvelope = $serializer->deserialize($serializedEnvelope, get_class($originalEnvelope));
+        $this->assertEquals($originalEnvelope, $deserializedEnvelope);
+    }
+
+    /**
+     * @test
+     */
+    public function it_fails_when_the_deserialized_object_is_of_the_wrong_type()
+    {
+        $expectedType = 'SimpleBus\Serialization\Tests\Fixtures\DummyMessage';
+        $message = new AnotherDummyMessage();
+        $serializer = new NativeObjectSerializer();
+        $serializedMessage = $serializer->serialize($message);
+
+        $this->expectException('\LogicException', $expectedType);
+        $serializer->deserialize($serializedMessage, $expectedType);
+    }
+}
